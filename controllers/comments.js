3caf98c2a9b05be6d9f3f39985043d0fcd0ec289
Megan Field <megan.field@hotmail.co.uk>
2018-02-06 14:43:50 +0000

    PUT request for voting up and down on comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,4 +1,13 @@
-const updateCommentVotes = () => {}
+const updateCommentVotes = () => {
+    const {comment_id} = req.params;
+    let {vote} = req.query;
+    let num;
+        if (vote === 'up') num = 1;
+        if (vote === 'down') num = -1;
+      
+    Comment.findByIdAndUpdate({_id: comment_id}, { $inc: {votes: num}})
+      .then(comment => res.send(comment));
+}
 
 const deleteCommentById = () => {}
 
