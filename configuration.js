328cb67d183a1969b88e0b04e1dc502ff987cfb9
Megan Field <megan.field@hotmail.co.uk>
2018-02-20 15:37:22 +0000

    configuration.js

---
+++ configuration.js
@@ -0,0 +1,12 @@
+module.exports = {
+  DB: {
+    test: 'mongodb://localhost/northcoders-news-api-test',
+    dev: 'mongodb://localhost/northcoders-news',
+    production: process.env.DB
+  },
+  PORT: {
+    test: 3090,
+    dev: 3000,
+    production: process.env.PORT
+  }
+}
