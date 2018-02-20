ec174c6013006aba5b83a02bbbd6e781bdd4580f
Megan Field <megan.field@hotmail.co.uk>
2018-02-20 15:57:17 +0000

    remove hardcoding of port value in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = require('./configuration').PORT['dev']
+const PORT = require('./configuration').PORT[process.env.NODE_ENV]
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`)
